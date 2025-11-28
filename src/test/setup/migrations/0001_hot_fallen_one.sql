64d372bc6bf05cb6ddeb17e25093144f5b2a503d
Tim Froehlich <timothyfroehlich@gmail.com>
2025-11-28 13:18:07 -0600

    fix: Move auth.users creation from migration to test setup

--- src/test/setup/migrations/0001_hot_fallen_one.sql
+++ src/test/setup/migrations/0001_hot_fallen_one.sql
@@ -1,6 +1,2 @@
-CREATE TABLE "auth"."users" (
-	"id" uuid PRIMARY KEY NOT NULL,
-	"email" text NOT NULL
-);
---> statement-breakpoint
+
 ALTER TABLE "issues" ADD COLUMN "priority" text DEFAULT 'low' NOT NULL;
