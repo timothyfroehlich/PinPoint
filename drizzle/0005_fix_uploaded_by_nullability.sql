cba24a959812cb29cb9adaa8a4235602f280c666
Tim Froehlich <timothyfroehlich@gmail.com>
2026-01-26 00:23:48 -0600

    fix(e2e): resolve regressions in auth and notification tests

--- drizzle/0005_fix_uploaded_by_nullability.sql
+++
@@ -1,3 +0,0 @@
--- Custom migration to ensure uploaded_by is nullable
--- This fixes issues where the table might have been created with NOT NULL in a previous version
-ALTER TABLE "issue_images" ALTER COLUMN "uploaded_by" DROP NOT NULL;
